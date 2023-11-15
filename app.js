1957bf2987af82136b8cbfb65cf33445083cbfea
Chanell Cousin <71571386+NellPrima@users.noreply.github.com>
2023-11-15 01:47:25 -0600

    started todo project

---
+++ app.js
@@ -0,0 +1,43 @@
+//Selectors
+const todoInput = document.querySelector('.todo_input');
+const todoButton = document.querySelector('.todo_bttn');
+const todoList = document.querySelector('.todo_list');
+
+
+//Event Listeners
+
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(event) {
+    //Prevent form from submitting
+    event.preventDefault();
+    //Todo aside
+    const todoAside = document.createElement("aside");
+    todoAside.classList.add('todo');
+
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo_item');
+    todoAside.appendChild(newTodo);
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete_btn");
+    todoAside.appendChild(completedButton);
+
+     //trash button
+     const trashButton = document.createElement('button');
+     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+     trashButton.classList.add("complete_btn");
+     todoAside.appendChild(trashButton);
+     //Append to list
+     todoList.appendChild(todoAside);
+
+
+ 
+
+
+
+}
